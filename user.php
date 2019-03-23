fe628ebb8ca5a4ca83f0440e16fa8a904f7e4ebf
KartikeyaMalimath <kartikeyapm@gmail.com>
2019-03-23 15:27:37 +0530

    user front End

--- user.php
+++ user.php
@@ -12,17 +12,34 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
 
 </head>
-<body>
-    
-    <div class="container">
-        <div class="Jumbotron">
-            
-        </div>
-    </div>
+<style>
+body {
+    background: #0f0c29;
+    background-image: -webkit-linear-gradient(to left, #24243e, #302b63, #0f0c29);
+    background-image: linear-gradient(to left, #24243e, #302b63, #0f0c29); 
+}
+</style>
 
+
+<body>
+    <div class="container">
+        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">  
+        <div class="cardcontain">
+            
+            <div class="card">
+                <div class="front">Click to flip</div>
+                <div class="back">Yo, what up?</div>
+            </div>
+        </div>
+</div>
+    <script  src="js/user.js"></script>
 </body>
 </html>
