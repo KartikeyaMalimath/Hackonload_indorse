c40292afc19cb83afe43030aa30dd95b5711f233
KartikeyaMalimath <kartikeyapm@gmail.com>
2019-03-23 20:08:48 +0530

    user UI and backend part1

--- user.php
+++ user.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
+
+<!--= =================================
+    Author : KARTIKEYA P MALIMATH
+=================================== -->
+
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Easy-Transit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="CSS/user.css">
 
@@ -25,6 +30,7 @@ body {
     background: #0f0c29;
     background-image: -webkit-linear-gradient(to left, #24243e, #302b63, #0f0c29);
     background-image: linear-gradient(to left, #24243e, #302b63, #0f0c29); 
+    padding-bottom: 50px;
 }
 </style>
 
@@ -32,13 +38,66 @@ body {
 <body>
     <div class="container">
         <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">  
+        <!-- CARD1 -->
         <div class="cardcontain">
-            
             <div class="card">
-                <div class="front">Click to flip</div>
-                <div class="back">Yo, what up?</div>
+                <div class="front"><h4>Welcome to Easy-Transit</br><hr>Enter your current location and bus number</h4></div>
+                <div class="back">
+                    <form action="functions/userreq.php">
+                        Enter Bus Number:</br>
+                        <input name="busno" type="number"></br>
+                        Enter your bus stop:</br>
+                        <input name="source" type="Text" ></br>
+                        <input name="submit" type="submit">
+                    </form>
+                </div>
             </div>
         </div>
+        <!-- card2 -->
+        <?php
+
+        include ('database/db.php');
+
+        if($_GET) {
+            $busID = $_GET['bus'];
+            $closesit = $_GET['sit'];
+            $closestand = $_GET['stand'];
+            $start = $_GET['start']; 
+
+            $sql = "SELECT * from bus where busid = '$busID'";
+            $result = $con->query($sql);
+            while($row =$result->fetch_assoc()) {
+                $maxsit = $row['sittingcap'];
+                $maxstand = $row['standingcap'];
+            }
+
+            $sql = "SELECT * from routes where busid = '$busID'";
+            $result = $con->query($sql);
+            while($row =$result->fetch_assoc()) {
+                $maxsit = $row['sittingcap'];
+                $maxstand = $row['standingcap'];
+            }
+
+            $availsit = $maxsit - $closesit;
+            $availstand = $maxstand - $closestand;
+            echo "
+                <div class='cardcontain'>
+                <div class='card2'>
+                    <div class='front'>
+                        <h4>Bus Number : ".$busID." opted stop : ".$start."</br>
+                        <hr>Available Seatings : ".$availsit."</br>
+                        <hr>Available Standing : ".$availstand."</br>
+                        <hr>Bus will arrive at : </h4>
+                    </div>
+                    <div class='back'>
+                        
+                    </div>
+                </div>
+            ";
+          }
+        ?>
+
+
 </div>
     <script  src="js/user.js"></script>
 </body>
